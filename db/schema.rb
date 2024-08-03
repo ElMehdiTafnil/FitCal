dc4586db02fc3e370f3cf179440275bca0d0ec7c
otmane aghoudid <othmane_aghoudid@hotmail.fr>
2024-08-03 13:08:05 +0100

    added some more home page style

--- db/schema.rb
+++ db/schema.rb
@@ -90,10 +90,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_20_112023) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "age"
-    t.integer "height"
-    t.integer "weight"
-    t.string "activity_level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
