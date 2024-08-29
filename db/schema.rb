7a554d7b9f65ccf1c77163e89ccc24475852245c
ElMehdiTafnil <elmehdi.tafnil@gmail.com>
2024-08-29 16:29:48 +0100

    database updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_17_102752) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_29_152033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_17_102752) do
     t.string "img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "objectives", force: :cascade do |t|
